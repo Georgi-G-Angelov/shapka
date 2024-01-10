27c736bdbc968c85e8a100760562d30ac609269c
Georgi Angelov <gangelov@microsoft.com>
2024-01-10 20:08:47 +0000

    more controllers

--- src/home_pages_controller.rs
+++ src/home_pages_controller.rs
@@ -1,5 +1,8 @@
-use std::path::{Path};
-use rocket::fs::{NamedFile};
+use std::path::Path;
+use chashmap::CHashMap;
+use rocket::{fs::NamedFile, State, response::content};
+
+use crate::game::Game;
 
 #[get("/home")]
 pub async fn home() -> Option<NamedFile> {
@@ -14,4 +17,65 @@ pub async fn join() -> Option<NamedFile> {
 #[get("/create")]
 pub async fn create() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/create.html")).await.ok()
+}
+
+
+#[get("/host/<game_id>/<name>")]
+pub async fn host(game_id: i32, name: &str, games: &State<CHashMap<i32, Game>>) -> Option<NamedFile> {
+    if !games.contains_key(&game_id) {
+        error!("Game does not exist");
+        return Option::<NamedFile>::None;
+    }
+    let game = games.get(&game_id).unwrap();
+    if !game.players
+        .lock()
+        .expect("locked game")
+        .contains(&name.to_string()) {
+        return Option::<NamedFile>::None;
+    }
+    NamedFile::open(Path::new("static/host.html")).await.ok()
+}
+
+#[get("/await/<game_id>/<name>")]
+pub async fn await_game(game_id: i32, name: &str, games: &State<CHashMap<i32, Game>>) -> Option<NamedFile> {
+    if !games.contains_key(&game_id) {
+        error!("Game does not exist");
+        return Option::<NamedFile>::None;
+    }
+    let game = games.get(&game_id).unwrap();
+    if !game.players
+        .lock()
+        .expect("locked game")
+        .contains(&name.to_string()) {
+        return Option::<NamedFile>::None;
+    }
+    NamedFile::open(Path::new("static/await.html")).await.ok()
+}
+
+#[get("/join_game/<game_id>/<name>")]
+pub async fn join_game(game_id: i32, name: &str, games: &State<CHashMap<i32, Game>>) -> content::RawJson<String> {
+    if games.contains_key(&game_id) {
+        let game = games.get(&game_id).unwrap();
+        if game.players
+            .lock()
+            .expect("locked game")
+            .contains(&name.to_string()) {
+
+            content::RawJson("Name already exists".to_string())
+
+        } else {
+            game.players
+                .lock()
+                .expect("locked game")
+                .push(name.to_string());
+
+            let _res = game.queue.send(name.to_string());
+
+            content::RawJson(game_id.to_string())
+
+        }
+    } else {
+        content::RawJson("Game not found".to_string())
+
+    }
 }
---
+++ src/words_controller.rs
@@ -0,0 +1,53 @@
+use rocket::State;
+use rocket::response::content;
+
+use chashmap::CHashMap;
+
+use crate::game::Game;
+
+#[get("/add_word/<game_id>/<name>/<word>")]
+pub fn add_word(game_id: i32, name: &str, word: &str, games: &State<CHashMap<i32, Game>>) -> content::RawJson<String>{
+    let word_limit: i32 = 4;
+
+    let game = games.get(&game_id).unwrap();
+    if !game.num_words_per_player
+            .lock()
+            .expect("locked num words per player")
+            .contains_key(name) {
+        game.num_words_per_player
+            .lock()
+            .expect("locked num words per player")
+            .insert(name.to_string(), 0);
+    }
+
+    let game = games.get(&game_id).unwrap();
+
+    if game.num_words_per_player
+            .lock()
+            .expect("locked num words per player")
+            .get(name).unwrap() < &word_limit {
+        game.words
+            .lock()
+            .expect("List of words locked")
+            .push(word.to_string());
+
+        let curr_words: i32 = *game.num_words_per_player
+                                .lock()
+                                .expect("locked num words per player")
+                                .get(name).unwrap();
+
+        game.num_words_per_player
+            .lock()
+            .expect("locked num words per player")
+            .insert(name.to_string(), curr_words + 1);
+
+        println!("{}", game.num_words_per_player
+            .lock()
+            .expect("locked num words per player")
+            .get(name).unwrap());
+
+        content::RawJson("Word added: ".to_owned() + word)
+    } else {
+        content::RawJson("You can't add more words".to_string())
+    }
+}
