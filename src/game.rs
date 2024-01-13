86e56fd9db9f7c723a0d9d6a6ced79738e9fe90a
Georgi Angelov <gangelov@microsoft.com>
2024-01-13 18:07:00 +0000

    Init teams on game start

--- src/game.rs
+++ src/game.rs
@@ -2,6 +2,8 @@ use std::sync::Mutex;
 use rocket::tokio::sync::broadcast::Sender;
 use std::collections::HashMap;
 use rocket::tokio::sync::broadcast::channel;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 pub struct Game {
     pub id: i32,
@@ -9,7 +11,20 @@ pub struct Game {
     pub players: Mutex<Vec<String>>,
     pub words: Mutex<Vec<String>>,
     pub words_per_player_limit: i32,
-    pub num_words_per_player: Mutex<HashMap<String, i32>>
+    pub num_words_per_player: Mutex<HashMap<String, i32>>,
+    pub game_state: Mutex<GameState>
+}
+
+pub struct GameState {
+    pub timer: i32, // number from 0 to 60
+    pub current_player: String,
+    pub num_words_guessed_per_team: HashMap<i32, i32>,
+    pub teams: Vec<Vec<String>>,
+    pub words_guessed: Vec<String>,
+    pub words_in_play: Vec<String>, // from 0 to 2 elements
+    pub words_to_guess: Vec<String>,
+    pub round: i32, // from 0 to 3
+    pub is_turn_active: bool
 }
 
 pub fn init_game(id: i32, owner_name: &str, words_per_player_limit: i32) -> Game {
@@ -20,7 +35,8 @@ pub fn init_game(id: i32, owner_name: &str, words_per_player_limit: i32) -> Game
         players: Mutex::new(vec![]),
         words: Mutex::new(vec![]),
         words_per_player_limit,
-        num_words_per_player: Mutex::new(HashMap::new())
+        num_words_per_player: Mutex::new(HashMap::new()),
+        game_state: Mutex::new(init_game_state())
     };
     game.players
         .lock()
@@ -28,4 +44,49 @@ pub fn init_game(id: i32, owner_name: &str, words_per_player_limit: i32) -> Game
         .push(owner_name.to_string());
 
     return game;
+}
+
+pub fn init_game_state() -> GameState {
+    return GameState {
+        timer: 60,
+        current_player: "".to_string(),
+        num_words_guessed_per_team: HashMap::new(),
+        teams: Vec::new(),
+        words_guessed: Vec::new(),
+        words_in_play: Vec::new(),
+        words_to_guess: Vec::new(),
+        round: 0,
+        is_turn_active: false
+    }
+}
+
+pub fn init_teams(game_state: &Mutex<GameState>, players: &Mutex<Vec<String>>) {
+    players
+        .lock()
+        .unwrap()
+        .shuffle(&mut thread_rng());
+
+    let mut is_first_player_in_team = true;
+    for player in players.lock().unwrap().iter() {
+        if is_first_player_in_team {
+            let mut team: Vec<String> = Vec::new();
+            team.push(player.to_string());
+            game_state.lock().unwrap().teams.push(team);
+            is_first_player_in_team = false;
+        } else {
+            game_state.lock().unwrap()
+                .teams
+                .last_mut().unwrap()
+                .push(player.to_string());
+            is_first_player_in_team = true;
+        }
+    }
+
+    for team in game_state.lock().unwrap().teams.iter() {
+        for player in team {
+            print!("{player}");
+            print!(" ");
+        }
+        println!("");
+    }
 }
--- src/turns_controller.rs
+++ src/turns_controller.rs
@@ -3,17 +3,22 @@ use rocket::State;
 
 use chashmap::CHashMap;
 
-use crate::game::Game;
+use crate::game::{Game, init_teams};
 
 #[get("/start_game/<game_id>")]
 pub async fn start_game(game_id: i32, games: &State<CHashMap<i32, Game>>) -> content::RawJson<String> {
     if games.contains_key(&game_id) {
         let game = games.get(&game_id).unwrap();
+        let game_state = &game.game_state;
+        let players = &game.players;
 
-            let event: String = "start_game".to_owned();
-            let _res = game.game_events.send(event.to_string());
+        // init_teams(&mut game);
+        init_teams(game_state, players);
 
-            content::RawJson(game_id.to_string())
+        let event: String = "start_game".to_owned();
+        let _res = game.game_events.send(event.to_string());
+
+        content::RawJson(game_id.to_string())
 
     } else {
         content::RawJson("Game not found".to_string())
