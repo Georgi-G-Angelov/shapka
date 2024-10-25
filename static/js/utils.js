45ee503ef4bf15809ff94f9b63c5d4ca258dbe11
Georgi Angelov <gangelov@microsoft.com>
2024-10-26 00:05:16 +0100

    Remove unused code

--- static/js/utils.js
+++ static/js/utils.js
@@ -161,18 +161,6 @@ function subscribe(uri) {
             } else if (message.startsWith(UNDO_GUESS_PREFIX)) {
                 wordsLeftInRound++;
                 updateWordsLeftInRound(wordsLeftInRound, totalNumWords);
-            } else if (message.startsWith(NEW_WORD_PREFIX)) {
-                // var player_word = message.substring(NEW_WORD_PREFIX.length);
-                // var player = player_word.split("/")[0];
-                // var word = player_word.split("/")[1];
-
-                // if (player == getPlayerName()) {
-                //     var ul = document.getElementById("words");
-                //     var li = document.createElement("li");
-                //     li.classList.add('word_element');
-                //     li.appendChild(document.createTextNode(word));
-                //     ul.appendChild(li);
-                // }
             }
         });
 
